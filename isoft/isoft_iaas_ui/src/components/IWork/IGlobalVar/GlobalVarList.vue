533def09c2f820a2a75ff034716bd950f3ecfbed
389093982 <389093982@qq.com>
2019-05-14 22:26:25 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/IWork/IGlobalVar/GlobalVarList.vue
+++ isoft/isoft_iaas_ui/src/components/IWork/IGlobalVar/GlobalVarList.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+
+    <ISimpleLeftRightRow style="margin-bottom: 10px;margin-right: 10px;">
+      <!-- left 插槽部分 -->
+      <!-- right 插槽部分 -->
+      <ISimpleSearch slot="right" @handleSimpleSearch="handleSearch"/>
+    </ISimpleLeftRightRow>
+
     <Table border :columns="columns1" :data="globalVars" size="small"></Table>
     <Page :total="total" :page-size="offset" show-total show-sizer :styles="{'text-align': 'center','margin-top': '10px'}"
           @on-change="handleChange" @on-page-size-change="handlePageSizeChange"/>
@@ -8,9 +15,12 @@
 
 <script>
   import {GlobalVarList} from "../../../api"
+  import ISimpleLeftRightRow from "../../Common/layout/ISimpleLeftRightRow"
+  import ISimpleSearch from "../../Common/search/ISimpleSearch"
 
   export default {
     name: "GlobalVarList",
+    components:{ISimpleLeftRightRow,ISimpleSearch},
     data(){
       return {
         // 当前页
@@ -41,6 +51,12 @@
       },
       handlePageSizeChange(pageSize){
         this.offset = pageSize;
+        this.refreshGlobalVarList();
+      },
+      handleSearch(data){
+        this.offset = 10;
+        this.current_page = 1;
+        this.search = data;
         this.refreshGlobalVarList();
       },
       refreshGlobalVarList:async function () {
