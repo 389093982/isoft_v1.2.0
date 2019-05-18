507ef3b0b6c615cc594b6010b9739c9c7066bd54
389093982 <389093982@qq.com>
2019-05-18 22:01:54 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/imodules/index.js
+++ isoft/isoft_iaas_ui/src/imodules/index.js
@@ -2,7 +2,7 @@ import {oneOf} from "../tools"
 import {checkSSOLogin} from "./sso"
 
 // 前台开启的模块 sso,ilearning,iwork
-const open_modules = ["iwork", "ilearning", "sso"];
+const open_modules = ["ilearning", "sso"];
 
 export function modulesCheck(moduleName) {
   return oneOf(moduleName, open_modules);
--- isoft/isoft_iaas_ui/src/router/index.js
+++ isoft/isoft_iaas_ui/src/router/index.js
@@ -2,19 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import {getISSORouters} from "./isso"
-import {getIWorkRouters} from "./iwork"
 import {getILearningRouters} from "./ilearning"
 import {modulesCheck} from "../imodules";
 
 Vue.use(Router);
 
 function getRootRouters () {
-  if(modulesCheck("iwork")){
-    return [{
-      path: '/',
-      redirect: '/iwork/workList'
-    }]
-  }
   if(modulesCheck("ilearning")){
     return [{
       path: '/',
@@ -27,7 +20,6 @@ function getRootRouters () {
 
 function getAllRouters() {
   let allRouters = [];
-  [].push.apply(allRouters, getIWorkRouters());
   [].push.apply(allRouters, getILearningRouters());
   [].push.apply(allRouters, getISSORouters());
   [].push.apply(allRouters, getRootRouters());
