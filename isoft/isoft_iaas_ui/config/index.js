ac94ae60e6ce4e8e5d0438468fddc64e7a9f59c9
389093982 <389093982@qq.com>
2019-05-19 08:59:41 +0800

    代码优化

--- isoft/isoft_iaas_ui/config/index.js
+++ isoft/isoft_iaas_ui/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': { // 匹配所有以 '/api'开头的请求路径
-        target: 'http://localhost:8086', // 代理目标的基础路径
+        target: 'http://localhost:8087', // 代理目标的基础路径
         changeOrigin: true, // 支持跨域
         pathRewrite: {// 路径重写
           '^/api': '/api'
