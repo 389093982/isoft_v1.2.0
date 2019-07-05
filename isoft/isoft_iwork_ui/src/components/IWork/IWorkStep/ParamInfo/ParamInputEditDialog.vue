c8d2c753e2aa2df7cbda073ffb0337254bca4400
389093982 <389093982@qq.com>
2019-07-06 00:43:36 +0800

    代码优化

--- isoft/isoft_iwork_ui/src/components/IWork/IWorkStep/ParamInfo/ParamInputEditDialog.vue
+++ isoft/isoft_iwork_ui/src/components/IWork/IWorkStep/ParamInfo/ParamInputEditDialog.vue
@@ -9,20 +9,7 @@
     :styles="{top: '20px'}">
     <Row>
       <Col span="7">
-        <Tabs type="card" :value="current_tab" @on-click="currentTabChanged">
-          <TabPane label="前置节点输出参数" name="tab_output">
-            <Scroll height="350">
-              <Tree :data="data1" show-checkbox ref="tree1" :render="renderContent"></Tree>
-            </Scroll>
-          </TabPane>
-          <TabPane label="快捷函数" name="tab_funcs">
-            <Scroll height="350">
-              <ul>
-                <Tree :data="data2" show-checkbox ref="tree2" :render="renderContent"></Tree>
-              </ul>
-            </Scroll>
-          </TabPane>
-        </Tabs>
+        <ParamInputEditDataSource ref="paramInputEditDataSource" :pre-pos-tree-node-arr="prePosTreeNodeArr"/>
       </Col>
       <Col span="3" style="text-align: center;margin-top: 100px;">
         <Button @click="appendData('parent')" style="margin-top: 10px;"><Icon type="ios-arrow-forward"></Icon>选择父节点</Button>
@@ -89,10 +76,11 @@
   import {ParseToMultiValue} from "../../../../api"
   import TemplateChooser from "./TemplateChooser"
   import {getMatchArrForString} from "../../../../tools"
+  import ParamInputEditDataSource from "./ParamInputEditDataSource"
 
   export default {
     name: "ParamInputEditDialog",
-    components:{ISimpleBtnTriggerModal,QuickFuncList,TemplateChooser},
+    components:{ISimpleBtnTriggerModal,QuickFuncList,TemplateChooser,ParamInputEditDataSource},
     data(){
       return {
         showFormModal:false,
@@ -105,63 +93,10 @@
         multiVals:[],         // 存储多值列表
         paramIndex:1,
         prePosTreeNodeArr:[],
-        funcs:this.GLOBAL.quick_funcs,
-        current_tab:'tab_output',
         variables:[],
       }
     },
     methods:{
-      renderContent (h, { root, node, data }) {
-        return h('span', {
-          style: {
-            display: 'inline-block',
-            width: '100%'
-          },
-          attrs: {
-            draggable:'true'
-          },
-          on:{
-            dragstart: () => this.handleDragStart(root, node, data),
-          }
-        }, [
-          h('span', [
-            h('Icon', {
-              props: {
-                type: 'ios-paper-outline'
-              },
-              style: {
-                marginRight: '8px'
-              }
-            }),
-            h('span', data.title)
-          ]),
-        ]);
-      },
-      handleDragStart(root, node, data){
-        const event = window.event||arguments[0];
-        // 获取父节点
-        var getParent = function (_root, _node) {
-          if(_root.find(el => el === _node)){
-            const parentKey = _root.find(el => el === _node).parent;
-            if(parentKey != null){
-              parent = _root.find(el => el.nodeKey === parentKey);
-              return parent;
-            }
-          }
-          return null;
-        };
-        // 获取当前节点对应的 title
-        var getCurrentTitle = function (_root, _node) {
-          var parent = getParent(_root, _node);
-          if(parent == null){
-            return _node.node.title;
-          }
-          return getCurrentTitle(_root, parent) + "." + _node.node.title;
-        };
-        var data = getCurrentTitle(root, node)  + ";\n";
-        // 传递数据
-        event.dataTransfer.setData("Text", data);
-      },
       handleDrop:function(){
         const event = window.event||arguments[0];
         // 取消冒泡
@@ -239,102 +174,19 @@
           this.prePosTreeNodeArr = result.prePosTreeNodeArr;
         }
       },
-      appendDataWithPrefix:function(prefix, item, chooseType){
-        // 没有子节点
-        if(item.children == null){
-          if(item.indeterminate == false){
-            if(item.checked){
-              // 将数据添加到右侧
-              this.inputTextData = this.inputTextData + prefix + ";\n";
-            }
-          }
-        }else{
-          // 有子节点
-          let items = item.children;
-          if(chooseType == 'parent' && item.indeterminate == false){
-            if(item.checked){
-              // 将数据添加到右侧
-              this.inputTextData = this.inputTextData + prefix + ";\n";
-            }
-          }else{
-            for(var i=0; i<items.length; i++){
-              let item = items[i];
-              this.appendDataWithPrefix(prefix + "." + item.title, item, chooseType);
-            }
-          }
-        }
-      },
       appendData:function (chooseType) {
-        if(this.current_tab == "tab_output"){
-          this.appendDataWithOutput(chooseType)
+        let datas = this.$refs.paramInputEditDataSource.getChooseDatas(chooseType);
+        if(Array.isArray(datas)){
+          for(var i=0; i<datas.length; i++){
+            this.inputTextData = this.inputTextData + datas[i];
+          }
         }else{
-          this.appendDataWithFuncs();
-        }
-      },
-      appendDataWithFuncs: function(){
-        let items = this.$refs.tree2.getCheckedAndIndeterminateNodes();
-        if(items.length > 0){
-          var item = items[0];
-          this.inputTextData = this.inputTextData + item.title + ";\n";
-          if(items.length > 1){
-            this.$Message.warning("只有第一个有效！");
-          }
-        }
-      },
-      appendDataWithOutput:function (chooseType) {
-
-        let items = this.$refs.tree1.getCheckedAndIndeterminateNodes();
-        for(var i=0; i<items.length; i++){
-          let item = items[i];
-          // 只统计以 $ 开头的数据
-          if(item.title.indexOf("$") != -1){
-            this.appendDataWithPrefix(item.title,item, chooseType);
-          }
+          this.inputTextData = this.inputTextData + datas;
         }
       },
       chooseTemplate:function (template) {
         this.inputTextData = this.inputTextData + template.template_value;
         this.$refs.templateModal.hideModal();
-      },
-      currentTabChanged:function (name) {
-        this.current_tab = name;
-      }
-    },
-    computed:{
-      data1:function () {
-        var appendChildrens = function (paramOutputSchemaTreeNode, node) {       // 父级节点对象、父级节点树元素
-          if(paramOutputSchemaTreeNode.NodeChildrens != null && paramOutputSchemaTreeNode.NodeChildrens.length > 0){
-            const arr = [];
-            for(var i=0; i<paramOutputSchemaTreeNode.NodeChildrens.length; i++) {
-              var childParamOutputSchemaTreeNode = paramOutputSchemaTreeNode.NodeChildrens[i];
-              var childNode = {title: childParamOutputSchemaTreeNode.NodeName,expand: false};
-              // 递归操作
-              appendChildrens(childParamOutputSchemaTreeNode, childNode);
-              arr.push(childNode);
-            }
-            node.children = arr;
-          }
-        };
-        // tree 对应的 arr
-        let treeArr = [];
-        for(var i=0; i<this.prePosTreeNodeArr.length; i++){
-          let preParamOutputSchemaTreeNode = this.prePosTreeNodeArr[i];
-          const topTreeNode = {
-            title: preParamOutputSchemaTreeNode.NodeName,
-            expand: false,
-          };
-          appendChildrens(preParamOutputSchemaTreeNode,topTreeNode);
-          treeArr.push(topTreeNode);
-        }
-        return treeArr;
-      },
-      data2:function () {
-        // tree 对应的 arr
-        let treeArr = [];
-        for(var i=0; i<this.funcs.length; i++){
-          treeArr.push({title:this.funcs[i].funcDemo});
-        }
-        return treeArr;
       },
     },
     watch: {
