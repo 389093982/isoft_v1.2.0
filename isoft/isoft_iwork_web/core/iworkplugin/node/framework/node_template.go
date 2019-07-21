b5eadd5e3b823d091750d3a81ed89f1cffe6f9c1
389093982 <389093982@qq.com>
2019-07-21 19:20:30 +0800

    代码优化

--- isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
+++ isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
@@ -1,6 +1,8 @@
 package framework
 
 import (
+	"isoft/isoft_iwork_web/core/iworkconst"
+	"isoft/isoft_iwork_web/core/iworkmodels"
 	"isoft/isoft_iwork_web/core/iworkplugin/node"
 	"isoft/isoft_iwork_web/models"
 )
@@ -12,4 +14,17 @@ type TemplateNode struct {
 
 func (this *TemplateNode) Execute(trackingId string) {
 
+}
+
+func (this *TemplateNode) GetDefaultParamInputSchema() *iworkmodels.ParamInputSchema {
+	paramMap := map[int][]string{
+		1: {iworkconst.STRING_PREFIX + "template_text", "模板文字"},
+		2: {iworkconst.STRING_PREFIX + "template_varName", "模板变量名称"},
+		3: {iworkconst.STRING_PREFIX + "template_varValue", "模板变量值"},
+	}
+	return this.BuildParamInputSchemaWithDefaultMap(paramMap)
+}
+
+func (this *TemplateNode) GetDefaultParamOutputSchema() *iworkmodels.ParamOutputSchema {
+	return this.BuildParamOutputSchemaWithSlice([]string{iworkconst.STRING_PREFIX + "template_text"})
 }
