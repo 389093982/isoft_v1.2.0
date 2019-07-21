449685ed4a98db55b855d7f2b22f19d757ed895d
389093982 <389093982@qq.com>
2019-07-21 21:30:06 +0800

    代码优化

--- isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
+++ isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
@@ -1,10 +1,13 @@
 package framework
 
 import (
+	"github.com/pkg/errors"
 	"isoft/isoft_iwork_web/core/iworkconst"
 	"isoft/isoft_iwork_web/core/iworkmodels"
 	"isoft/isoft_iwork_web/core/iworkplugin/node"
 	"isoft/isoft_iwork_web/models"
+	"strings"
+	"text/template"
 )
 
 type TemplateNode struct {
@@ -12,15 +15,51 @@ type TemplateNode struct {
 	WorkStep *models.WorkStep
 }
 
-func (this *TemplateNode) Execute(trackingId string) {
+type stringWriter struct {
+	s string
+}
 
+func (this *stringWriter) Write(p []byte) (n int, err error) {
+	this.s += string(p)
+	return len(p), nil
+}
+
+func parseToDataMap(template_varName string, template_varValue []interface{}) map[string]interface{} {
+	dataMap := make(map[string]interface{}, 0)
+	names := strings.Split(template_varName, ",")
+	if len(names) != len(template_varValue) {
+		panic("miss match length error for template_varName and template_varValue")
+	}
+	for index, name := range names {
+		dataMap[strings.TrimSpace(name)] = template_varValue[index]
+	}
+	return dataMap
+}
+
+func (this *TemplateNode) Execute(trackingId string) {
+	template_text := this.TmpDataMap[iworkconst.STRING_PREFIX+"template_text"].(string)
+	template_varName := this.TmpDataMap[iworkconst.STRING_PREFIX+"template_varName"].(string)
+	template_varValue := this.TmpDataMap[iworkconst.COMPLEX_PREFIX+"template_varValue"].([]interface{})
+	dataMap := parseToDataMap(template_varName, template_varValue)
+	tmpl, err := template.New("template").Parse(template_text)
+	if err != nil {
+		panic(errors.Wrap(err, "模板语法错误!"))
+	}
+	w := &stringWriter{}
+	err = tmpl.ExecuteTemplate(w, "template", dataMap)
+	if err != nil {
+		panic(err)
+	}
+	this.DataStore.CacheDatas(this.WorkStep.WorkStepName, map[string]interface{}{
+		iworkconst.STRING_PREFIX + "template_text": w.s,
+	})
 }
 
 func (this *TemplateNode) GetDefaultParamInputSchema() *iworkmodels.ParamInputSchema {
 	paramMap := map[int][]string{
 		1: {iworkconst.STRING_PREFIX + "template_text", "模板文字"},
 		2: {iworkconst.STRING_PREFIX + "template_varName", "模板变量名称"},
-		3: {iworkconst.STRING_PREFIX + "template_varValue", "模板变量值"},
+		3: {iworkconst.COMPLEX_PREFIX + "template_varValue", "模板变量值"},
 	}
 	return this.BuildParamInputSchemaWithDefaultMap(paramMap)
 }
