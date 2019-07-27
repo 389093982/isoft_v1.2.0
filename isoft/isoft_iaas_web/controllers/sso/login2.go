6190d15b30e7b19525597493a83715abbf13cf5c
389093982 <389093982@qq.com>
2019-07-27 23:51:54 +0800

    代码优化

--- isoft/isoft_iaas_web/controllers/sso/login2.go
+++ isoft/isoft_iaas_web/controllers/sso/login2.go
@@ -25,3 +25,24 @@ func (this *LoginController) PostRegist2() {
 	}
 	this.ServeJSON()
 }
+
+func (this *LoginController) PostLogin2() {
+	paramMap := map[string]interface{}{
+		"username":        this.Input().Get("username"),
+		"passwd":          this.Input().Get("passwd"),
+		"CreatedBy":       "SYSTEM",
+		"CreatedTime":     time.Now(),
+		"LastUpdatedBy":   "SYSTEM",
+		"LastUpdatedTime": time.Now(),
+	}
+	url := "http://localhost:8086/api/iwork/httpservice/PostLogin2"
+	headerMap := map[string]interface{}{}
+	result := make(map[string]interface{})
+	err := httputil.DoHttpRequestAndParseToObj(url, "post", paramMap, headerMap, &result)
+	if err == nil {
+		this.Data["json"] = &result
+	} else {
+		this.Data["json"] = &map[string]interface{}{"status": "ERROR", "errorMsg": err.Error()}
+	}
+	this.ServeJSON()
+}
