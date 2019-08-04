4c4a879b68753d5de05e98259c671eff87f933f3
389093982 <389093982@qq.com>
2019-08-04 21:56:30 +0800

    代码优化

--- isoft/isoft_iwork_ui/src/components/IWork/IModule/IFilterList.vue
+++ isoft/isoft_iwork_ui/src/components/IWork/IModule/IFilterList.vue
@@ -9,8 +9,10 @@
     <ul>
       <li v-for="module in modules" style="list-style: none;margin-top: 20px;">
         <Tag><span>{{module.module_name}}</span></Tag>
-        <CheckboxGroup>
-          <Checkbox :label="moduleWork.work_name" v-for="moduleWork in works" v-if="moduleWork.module_name == module.module_name"></Checkbox>
+        <Button type="success" size="small" @click="handleCheckAll(module.module_name)">全选</Button>
+        <CheckboxGroup v-model="checkedWorks">
+          <Checkbox :label="moduleWork.work_name" v-for="moduleWork in works" v-if="moduleWork.module_name == module.module_name"
+            ></Checkbox>
         </CheckboxGroup>
       </li>
     </ul>
@@ -31,6 +33,7 @@
         works: [],
         moduleWorks:[],
         current_filter_id:-1,
+        checkedWorks:[],
       }
     },
     methods:{
@@ -42,6 +45,26 @@
           this.works = result.works;
         }
       },
+      handleCheckAll:function (module_name) {
+        let work_names = this.works.filter(work => work.module_name == module_name).map(work => work.work_name);
+        var allInflag = true;
+        for(var i=0; i<work_names.length; i++){
+          let work_name = work_names[i];
+          if(!oneOf(work_name, this.checkedWorks)){
+            allInflag = false;
+            break;
+          }
+        }
+        if(allInflag){
+          this.checkedWorks = this.checkedWorks.filter(checkWork => !oneOf(checkWork, work_names));
+        }else{
+          let addWorks = work_names.filter(work_name => !oneOf(work_name, this.checkedWorks));
+          for(var i=0; i<addWorks.length; i++){
+            let work_name = addWorks[i];
+            this.checkedWorks.push(work_name);
+          }
+        }
+      }
     },
     mounted(){
       this.refreshFilterList();
