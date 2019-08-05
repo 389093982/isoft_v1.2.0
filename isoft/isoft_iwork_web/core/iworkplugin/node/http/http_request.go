81371ed8c602273a052c833c29e256430b5eec8f
389093982 <389093982@qq.com>
2019-08-05 23:26:41 +0800

    代码优化

--- isoft/isoft_iwork_web/core/iworkplugin/node/http/http_request.go
+++ isoft/isoft_iwork_web/core/iworkplugin/node/http/http_request.go
@@ -18,12 +18,19 @@ type HttpRequestParserNode struct {
 
 func (this *HttpRequestParserNode) Execute(trackingId string) {
 	request := this.Dispatcher.TmpDataMap[iworkconst.HTTP_REQUEST_OBJECT].(*http.Request)
-	headers := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"headers", this.WorkStep).(string)
+	headers := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"headers?", this.WorkStep).(string)
 	headerSlice := strings.Split(headers, ",")
 	paramMap := make(map[string]interface{}, 0)
 	for _, header := range headerSlice {
 		headerVal := request.Header.Get(header)
 		paramMap[header] = headerVal
+	}
+	cookies := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"cookies?", this.WorkStep).(string)
+	cookieSlice := strings.Split(cookies, ",")
+	for _, cookie := range cookieSlice {
+		if cookieVal, err := request.Cookie(cookie); err == nil {
+			paramMap[cookie] = cookieVal
+		}
 	}
 	paramMap["ip"] = httputil.GetClientIP(request)
 	// 将数据数据存储到数据中心
@@ -32,7 +39,8 @@ func (this *HttpRequestParserNode) Execute(trackingId string) {
 
 func (this *HttpRequestParserNode) GetDefaultParamInputSchema() *iworkmodels.ParamInputSchema {
 	paramMap := map[int][]string{
-		1: {iworkconst.STRING_PREFIX + "headers", "解析的请求头参数,多个参数使用逗号分隔"},
+		1: {iworkconst.STRING_PREFIX + "headers?", "解析的请求头参数,多个参数使用逗号分隔"},
+		2: {iworkconst.STRING_PREFIX + "cookies?", "解析的 cookies 参数,多个参数使用逗号分隔"},
 	}
 	return this.BuildParamInputSchemaWithDefaultMap(paramMap)
 }
@@ -43,14 +51,22 @@ func (this *HttpRequestParserNode) GetDefaultParamOutputSchema() *iworkmodels.Pa
 
 func (this *HttpRequestParserNode) GetRuntimeParamOutputSchema() *iworkmodels.ParamOutputSchema {
 	pos := &iworkmodels.ParamOutputSchema{}
-	headers := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"headers", this.WorkStep).(string)
-	headerSlice := strings.Split(headers, ",")
-	items := make([]iworkmodels.ParamOutputSchemaItem, 0)
-	for _, header := range headerSlice {
-		items = append(items, iworkmodels.ParamOutputSchemaItem{
-			ParamName: header,
-		})
-	}
-	pos.ParamOutputSchemaItems = append(pos.ParamOutputSchemaItems, items...)
+	headers := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"headers?", this.WorkStep).(string)
+	cookies := param.GetStaticParamValueWithStep(iworkconst.STRING_PREFIX+"cookies?", this.WorkStep).(string)
+	pos.ParamOutputSchemaItems = append(pos.ParamOutputSchemaItems, parseToItems(headers, "header_")...)
+	pos.ParamOutputSchemaItems = append(pos.ParamOutputSchemaItems, parseToItems(cookies, "cookie_")...)
 	return pos
+}
+
+func parseToItems(paramStr, paramPrefix string) []iworkmodels.ParamOutputSchemaItem {
+	items := make([]iworkmodels.ParamOutputSchemaItem, 0)
+	if paramStr != "" {
+		paramSlice := strings.Split(paramStr, ",")
+		for _, param := range paramSlice {
+			items = append(items, iworkmodels.ParamOutputSchemaItem{
+				ParamName: paramPrefix + param,
+			})
+		}
+	}
+	return items
 }
