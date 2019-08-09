125b354d98d9163263c4f47186055d607d41c224
389093982 <389093982@qq.com>
2019-08-09 21:36:05 +0800

    代码优化

--- isoft/isoft_iwork_web/main.go
+++ isoft/isoft_iwork_web/main.go
@@ -11,11 +11,19 @@ import (
 	_ "isoft/isoft_iwork_web/startup/logger"
 	_ "isoft/isoft_iwork_web/startup/sysconfig"
 	"isoft/isoft_iwork_web/startup/task"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 
 
 func main() {
+	go func() {
+		// http://127.0.0.1:10000/debug/pprof/
+		log.Println(http.ListenAndServe("localhost:10000", nil))
+	}()
+
 	beego.InsertFilter("/api/iwork/httpservice/*", beego.BeforeExec, filter.FilterFunc)
 
 	iworkpool.LoadAndCachePool()
