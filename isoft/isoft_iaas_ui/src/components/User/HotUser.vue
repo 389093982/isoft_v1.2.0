3bc90901273d5cde8fadc3bb4bab0e4b2e28c7fc
389093982 <389093982@qq.com>
2019-08-24 19:37:01 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/User/HotUser.vue
+++ isoft/isoft_iaas_ui/src/components/User/HotUser.vue
@@ -2,11 +2,17 @@
   <div>
     <a>热门用户</a>
     <a>积分排行榜</a>
-
     <Row v-for="user in users">
-      <img width="30" height="30" v-if="user.small_icon" :src="user.small_icon">
-      <img width="30" height="30" v-else src="../../../src/assets/sso/default_user_small_icon.jpg">
-      {{user.user_name}}
+      <Col span="4">
+        <img width="30" height="30" v-if="user.small_icon" :src="user.small_icon">
+        <img width="30" height="30" v-else src="../../../src/assets/sso/default_user_small_icon.jpg">
+      </Col>
+      <Col span="10">
+        {{user.user_name}}
+      </Col>
+      <Col span="10" class="small_font_size">
+        用户积分数：{{user.user_points}}
+      </Col>
     </Row>
   </div>
 </template>
@@ -36,5 +42,7 @@
 </script>
 
 <style scoped>
-
+.small_font_size {
+  font-size: 12px;
+}
 </style>
