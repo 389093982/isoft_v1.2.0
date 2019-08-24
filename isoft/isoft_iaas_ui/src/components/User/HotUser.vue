023c26e3b562dd16876f1214f414a3d862cd26a8
389093982 <389093982@qq.com>
2019-08-24 19:21:33 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/User/HotUser.vue
+++ isoft/isoft_iaas_ui/src/components/User/HotUser.vue
@@ -3,7 +3,11 @@
     <a>热门用户</a>
     <a>积分排行榜</a>
 
-    <Row v-for="user in users">{{user.user_name}}</Row>
+    <Row v-for="user in users">
+      <img width="30" height="30" v-if="user.small_icon" :src="user.small_icon">
+      <img width="30" height="30" v-else src="../../../src/assets/sso/default_user_small_icon.jpg">
+      {{user.user_name}}
+    </Row>
   </div>
 </template>
 
