b16f81804e951aab9a8c30e9cb3b894b61b61fad
389093982 <389093982@qq.com>
2019-08-26 23:48:56 +0800

    代码优化

---
+++ isoft/isoft_iaas_ui/src/components/Background/CMS/EditElement.vue
@@ -0,0 +1,96 @@
+<template>
+  <ISimpleConfirmModal ref="modal" :modal-width="1000" @handleSubmit="handleSubmit">
+    <IBaseChooser ref="placement_chooser" chooser-title="占位符选择">
+      <Placement :chooserMode="true" @choosePlacement="choosePlacement"/>
+    </IBaseChooser>
+
+    <Form ref="formInline" :model="formInline" :rules="ruleInline" :label-width="100">
+      <Row>
+        <Col span="12">
+          <FormItem label="占位符">
+            <Input type="text" readonly="readonly" v-model="formInline.placement" placeholder="placement" style="width: 80%;"/>
+            <Button type="success" @click="$refs.placement_chooser.showModal()">选择</Button>
+          </FormItem>
+          <FormItem prop="title" label="标题">
+            <Input type="text" v-model="formInline.title" placeholder="title" style="width: 80%;"/>
+          </FormItem>
+          <FormItem prop="content"  label="内容">
+            <Input type="text" v-model="formInline.content" placeholder="content" style="width: 80%;"/>
+          </FormItem>
+        </Col>
+        <Col span="12">
+          <FormItem prop="imgpath"  label="图片">
+            <Input type="text" readonly="readonly" v-model="formInline.imgpath" placeholder="imgpath" style="width: 80%;"/>
+            <IFileUpload @uploadComplete="uploadComplete" action="/api2/iwork/fileUpload/fileUpload" uploadLabel="上传"/>
+          </FormItem>
+          <FormItem prop="linked_refer"  label="链接关键词">
+            <Input type="text" v-model="formInline.linked_refer" placeholder="linked_refer" style="width: 80%;"/>
+          </FormItem>
+        </Col>
+      </Row>
+    </Form>
+  </ISimpleConfirmModal>
+</template>
+
+<script>
+  import ISimpleConfirmModal from "../../Common/modal/ISimpleConfirmModal"
+  import IBaseChooser from "../../Common/IBaseChooser"
+  import Placement from "./Placement"
+  import IFileUpload from "../../IFile/IFileUpload"
+  import {AddElement} from "../../../api"
+
+  export default {
+    name: "EditElement",
+    components:{IBaseChooser,Placement,IFileUpload,ISimpleConfirmModal},
+    data(){
+      return {
+        formInline: {
+          placement:'',
+          title: '',
+          content: '',
+          imgpath: '',
+          linked_refer: '',
+        },
+        ruleInline: {
+          title: [
+            { required: true, message: 'Please fill in the title.', trigger: 'blur' },
+          ],
+        }
+      }
+    },
+    methods:{
+      showModal:function(){
+        this.$refs.modal.showModal();
+      },
+      handleSubmit() {
+        this.$refs['formInline'].validate(async (valid) => {
+          if (valid) {
+            const result = await AddElement(this.formInline.placement, this.formInline.title, this.formInline.content,
+              this.formInline.imgpath, this.formInline.linked_refer);
+            if(result.status=="SUCCESS"){
+              this.$emit("refreshElementList");
+              this.$Message.success('提交成功!');
+            }else{
+              this.$Message.error('提交失败!' + result.errorMsg);
+            }
+          } else {
+            this.$Message.error('校验不通过!');
+          }
+        })
+      },
+      choosePlacement:function (placement_name) {
+        this.formInline.placement = placement_name;
+        this.$refs.placement_chooser.hideModal();
+      },
+      uploadComplete: function (result) {
+        if(result.status == "SUCCESS"){
+          this.formInline.imgpath = result.fileServerPath;
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- isoft/isoft_iaas_ui/src/components/Background/CMS/Element.vue
+++ isoft/isoft_iaas_ui/src/components/Background/CMS/Element.vue
@@ -1,40 +1,12 @@
 <template>
   <div>
-    <IBaseChooser ref="placement_chooser" chooser-title="占位符选择">
-      <Placement :chooserMode="true" @choosePlacement="choosePlacement"/>
-    </IBaseChooser>
+    <EditElement ref="editElement"/>
 
-    <Form ref="formInline" :model="formInline" :rules="ruleInline" :label-width="100">
-      <Row>
-        <Col span="12">
-          <FormItem label="占位符">
-            <Input type="text" readonly="readonly" v-model="formInline.placement" placeholder="placement" style="width: 80%;"/>
-            <Button type="success" @click="$refs.placement_chooser.showModal()">选择</Button>
-          </FormItem>
-          <FormItem prop="title" label="标题">
-            <Input type="text" v-model="formInline.title" placeholder="title" style="width: 80%;"/>
-          </FormItem>
-          <FormItem prop="content"  label="内容">
-            <Input type="text" v-model="formInline.content" placeholder="content" style="width: 80%;"/>
-          </FormItem>
-        </Col>
-        <Col span="12">
-          <FormItem prop="imgpath"  label="图片">
-            <Input type="text" readonly="readonly" v-model="formInline.imgpath" placeholder="imgpath" style="width: 80%;"/>
-            <IFileUpload @uploadComplete="uploadComplete" action="/api2/iwork/fileUpload/fileUpload" uploadLabel="上传"/>
-          </FormItem>
-          <FormItem prop="linked_refer"  label="链接关键词">
-            <Input type="text" v-model="formInline.linked_refer" placeholder="linked_refer" style="width: 80%;"/>
-          </FormItem>
-          <FormItem>
-            <Button type="primary" @click="handleSubmit('formInline')">提交</Button>
-          </FormItem>
-        </Col>
-      </Row>
-    </Form>
-
-    <!-- right 插槽部分 -->
-    <ISimpleSearch @handleSimpleSearch="handleSearch"/>
+    <ISimpleLeftRightRow>
+      <Button type="success" slot="left" @click="addElement">新增</Button>
+      <!-- right 插槽部分 -->
+      <ISimpleSearch slot="right" @handleSimpleSearch="handleSearch"/>
+    </ISimpleLeftRightRow>
 
     <Table :columns="columns1" :data="elements" size="small"></Table>
     <Page :total="total" :page-size="offset" show-total show-sizer :styles="{'text-align': 'center','margin-top': '10px'}"
@@ -43,16 +15,15 @@
 </template>
 
 <script>
-  import {FilterElements,AddElement,UpdateElementStatus} from "../../../api"
-  import IFileUpload from "../../IFile/IFileUpload"
-  import IBaseChooser from "../../Common/IBaseChooser"
+  import {FilterElements,UpdateElementStatus} from "../../../api"
   import ISimpleSearch from "../../Common/search/ISimpleSearch"
-  import Placement from "./Placement"
   import MultiClickButton from "../../Common/button/MultiClickButton"
+  import ISimpleLeftRightRow from "../../Common/layout/ISimpleLeftRightRow"
+  import EditElement from "./EditElement"
 
   export default {
     name: "Element",
-    components:{IFileUpload,IBaseChooser,Placement,MultiClickButton,ISimpleSearch},
+    components:{ISimpleLeftRightRow,MultiClickButton,ISimpleSearch, EditElement},
     data () {
       var _this = this;
       return {
@@ -133,37 +104,11 @@
             }
           },
         ],
-        formInline: {
-          placement:'',
-          title: '',
-          content: '',
-          imgpath: '',
-          linked_refer: '',
-        },
-        ruleInline: {
-          title: [
-            { required: true, message: 'Please fill in the title.', trigger: 'blur' },
-          ],
-        }
       }
     },
     methods: {
-      handleSubmit(name) {
-        this.$refs[name].validate(async (valid) => {
-          if (valid) {
-            const result = await AddElement(this.formInline.placement, this.formInline.title, this.formInline.content,
-              this.formInline.imgpath, this.formInline.linked_refer);
-            if(result.status=="SUCCESS"){
-              this.refreshElementList();
-              this.$Message.success('提交成功!');
-            }else{
-              this.$Message.error('提交失败!' + result.errorMsg);
-            }
-
-          } else {
-            this.$Message.error('校验不通过!');
-          }
-        })
+      addElement:function(){
+        this.$refs.editElement.showModal();
       },
       refreshElementList:async function () {
         const result = await FilterElements(this.offset, this.current_page, this.search);
@@ -179,15 +124,6 @@
       handlePageSizeChange(pageSize){
         this.offset = pageSize;
         this.refreshElementList();
-      },
-      uploadComplete: function (result) {
-        if(result.status == "SUCCESS"){
-          this.formInline.imgpath = result.fileServerPath;
-        }
-      },
-      choosePlacement:function (placement_name) {
-        this.formInline.placement = placement_name;
-        this.$refs.placement_chooser.hideModal();
       },
       handleSearch(data){
         this.search = data;
