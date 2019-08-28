20d08634df322706c66778ac160935aecf73e1ba
389093982 <389093982@qq.com>
2019-08-28 23:32:23 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/IBlog/BlogList.vue
+++ isoft/isoft_iaas_ui/src/components/IBlog/BlogList.vue
@@ -14,7 +14,7 @@
             </p>
             <p>
               <Row>
-                <Col span="18">
+                <Col span="17">
                   <!-- 作者详情 -->
                   <router-link :to="{path:'/iblog/author',query:{author:searchblog.author}}">{{searchblog.author}}</router-link>
                   发布于:<Time :time="searchblog.created_time" type="datetime" style="color:red;"/>&nbsp;
@@ -23,14 +23,19 @@
                     <span style="color: #3399ea;">{{ searchblog.catalog_name }}</span>
                   </router-link>
                 </Col>
-                <Col span="3">
+                <Col span="2">
                   <router-link :to="{path:'/iblog/blog_detail',query:{blog_id:searchblog.id}}">
                     <span style="color: red;">{{searchblog.views}}</span>阅读
                   </router-link>
                 </Col>
-                <Col span="3">
+                <Col span="2">
                   <router-link :to="{path:'/iblog/blog_detail',query:{blog_id:searchblog.id}}">
-                    <a><span style="color: red;">0</span>条评论</a>
+                    <span style="color: red;">0</span>条评论
+                  </router-link>
+                </Col>
+                <Col span="3">
+                  <router-link :to="{path:'/iblog/blog_add'}">
+                    我也要发布
                   </router-link>
                 </Col>
               </Row>
