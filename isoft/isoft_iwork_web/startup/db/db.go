60a78beb1bc32efe79f57aa29539f8f9171694a9
389093982 <389093982@qq.com>
2019-09-02 21:58:06 +0800

    代码优化

--- isoft/isoft_iwork_web/startup/db/db.go
+++ isoft/isoft_iwork_web/startup/db/db.go
@@ -28,8 +28,8 @@ func init() {
 
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", Dsn)
-	orm.SetMaxIdleConns("default", 1000) // SetMaxIdleConns用于设置闲置的连接数
-	orm.SetMaxOpenConns("default", 2000) // SetMaxOpenConns用于设置最大打开的连接数,默认值为0表示不限制
+	orm.SetMaxIdleConns("default", 100) // SetMaxIdleConns用于设置闲置的连接数
+	orm.SetMaxOpenConns("default", 200) // SetMaxOpenConns用于设置最大打开的连接数,默认值为0表示不限制
 	db, _ := orm.GetDB("default")
 	db.SetConnMaxLifetime(100)
 
