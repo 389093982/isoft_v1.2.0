5157c59ca1409a9530850452aa81379ac20438a9
389093982 <389093982@qq.com>
2019-09-05 23:47:21 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/ILearning/Site/CommunicationGroup.vue
+++ isoft/isoft_iaas_ui/src/components/ILearning/Site/CommunicationGroup.vue
@@ -1,18 +1,19 @@
 <template>
-  <div style="border: 1px #dbdbdb solid;margin-left: 5px;margin-top: 5px;padding: 15px;">
+  <ElementsLoader :placement_name="placement_name" @onLoadElement="onLoadElement"
+                   style="border: 1px #dbdbdb solid;margin-left: 5px;margin-top: 5px;padding: 15px;">
     <span style="font-size: 16px;">{{placement_label}}</span>
     <ul style="list-style: none;">
       <li v-for="element in elements">{{element.content}}</li>
     </ul>
-  </div>
+  </ElementsLoader>
 </template>
 
 <script>
-  import {FilterElementByPlacement} from "../../../api"
-  import {checkEmpty} from "../../../tools"
+  import ElementsLoader from "../../Background/CMS/ElementsLoader"
 
   export default {
     name: "CommunicationGroup",
+    components:{ElementsLoader},
     data(){
       return {
         elements:[],
@@ -26,19 +27,11 @@
       }
     },
     methods:{
-      refreshElement: async function () {
-        if(!checkEmpty(this.placement_name)){
-          const result = await FilterElementByPlacement(this.placement_name);
-          if(result.status == "SUCCESS"){
-            this.placement_label = result.placement.placement_label;
-            this.elements = result.elements;
-          }
-        }
+      onLoadElement:function (placement_label, elements) {
+        this.placement_label = placement_label;
+        this.elements = elements;
       }
     },
-    mounted(){
-      this.refreshElement();
-    }
   }
 </script>
 
