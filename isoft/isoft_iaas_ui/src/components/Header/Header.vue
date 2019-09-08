87adccb4f7bafa76f5bfb002dc461d8328727d57
389093982 <389093982@qq.com>
2019-09-08 21:35:58 +0800

    代码优化

--- isoft/isoft_iaas_ui/src/components/Header/Header.vue
+++ isoft/isoft_iaas_ui/src/components/Header/Header.vue
@@ -8,7 +8,7 @@
             精品应用
           </template>
           <MenuGroup title="博客天地">
-            <MenuItem name="1-1"><router-link to="/iblog/blog_list">热门博文推荐</router-link></MenuItem>
+            <MenuItem name="1-1"><router-link to="/iblog/blog_list">博客天地</router-link></MenuItem>
           </MenuGroup>
           <MenuGroup title="在线学习系统">
             <MenuItem name="1-5"><router-link to="/ilearning/index">精品课程</router-link></MenuItem>
