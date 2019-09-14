01e17602e8f1e28f59eac4ec6d3c0d05c26b0798
389093982 <389093982@qq.com>
2019-09-14 10:15:09 +0800

    代码优化

--- isoft/isoft_iwork_web/routers/commentsRouter_.go
+++ isoft/isoft_iwork_web/routers/commentsRouter_.go
@@ -63,6 +63,15 @@ func init() {
 
 	beego.GlobalControllerRouter["isoft/isoft_iwork_web/controllers:WorkController"] = append(beego.GlobalControllerRouter["isoft/isoft_iwork_web/controllers:WorkController"],
 		beego.ControllerComments{
+			Method:           "LoadValidateResult",
+			Router:           `/api/iwork/loadValidateResult`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["isoft/isoft_iwork_web/controllers:WorkController"] = append(beego.GlobalControllerRouter["isoft/isoft_iwork_web/controllers:WorkController"],
+		beego.ControllerComments{
 			Method:           "ToggleSqlMigrateEffective",
 			Router:           `/api/iwork/toggleSqlMigrateEffective`,
 			AllowHTTPMethods: []string{"post"},
