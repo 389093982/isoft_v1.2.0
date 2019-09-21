dbc74f58fd09c5aa8c086aa1b0140df303815a9e
389093982 <389093982@qq.com>
2019-09-21 18:29:17 +0800

    代码优化111

--- isoft/isoft_iaas_ui/src/components/IFound/FoundList.vue
+++ isoft/isoft_iaas_ui/src/components/IFound/FoundList.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    AAAAAAAAAAAAAAAAAAAAAAAAA
+  <div style="padding:20px;background-color: #fff;border: 1px solid #e6e6e6;border-radius: 4px;">
+    <div><IndexCarousel/></div>
+    <div><IBeautifulTabLink/></div>
+    <div>
+      <Row :gutter="10">
+        <Col span="12"><IndexCarousel/></Col>
+        <Col span="12"><IndexCarousel/></Col>
+      </Row>
+    </div>
   </div>
 </template>
 
 <script>
+  import IndexCarousel from "../ILearning/IndexCarousel";
+  import IBeautifulTabLink from "../Common/link/IBeautifulTabLink";
   export default {
-    name: "FoundList"
+    name: "FoundList",
+    components: {IBeautifulTabLink, IndexCarousel}
   }
 </script>
 
