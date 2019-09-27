647c9fee55c53241c353b57506aede03536df68b
389093982 <389093982@qq.com>
2019-09-27 20:08:42 +0800

    代码优化111

--- isoft/isoft_iwork_web/controllers/ifileupload.go
+++ isoft/isoft_iwork_web/controllers/ifileupload.go
@@ -11,14 +11,13 @@ import (
 	"isoft/isoft_iwork_web/core/iworkrun"
 	"isoft/isoft_iwork_web/core/iworkutil/errorutil"
 	"path"
-	"strings"
 )
 
 func (this *WorkController) FileUpload() {
+	defer this.ServeJSON()
 	defer func() {
 		if err := recover(); err != nil {
 			this.Data["json"] = &map[string]string{"status": "ERROR", "errorMsg": errorutil.ToError(err).Error()}
-			this.ServeJSON()
 		}
 	}()
 	var (
@@ -63,23 +62,17 @@ func (this *WorkController) FileUpload() {
 		"fileServerPath": tempFileServerPath,
 		"errorMsg":       errorMsg,
 	}
-	this.ServeJSON()
 }
 
 func (this *WorkController) saveFile() (tempFileName, fileName, tempFilePath string) {
-	tempFileName = stringutil.RandomUUID()
 	// 判断是否是文件上传
 	f, h, err := this.GetFile("file")
 	checkError(err)
+	tempFileName = stringutil.RandomUUID() + path.Ext(h.Filename)
 	//得到文件的名称
 	fileName = h.Filename
-	arr := strings.Split(fileName, ":")
-	if len(arr) > 1 {
-		index := len(arr) - 1
-		fileName = arr[index]
-	}
 	//关闭上传的文件，不然的话会出现临时文件不能清除的情况
-	f.Close()
+	defer f.Close()
 	//保存文件到指定的位置,static/uploadfile,这个是文件的地址,第一个static前面不要有/
 	tempFilePath = path.Join(beego.AppConfig.String("file.server"), tempFileName)
 	err = this.SaveToFile("file", tempFilePath)
