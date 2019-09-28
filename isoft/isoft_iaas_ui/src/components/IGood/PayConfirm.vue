fe0eb06890b6aeca86551c5dbc3ef92c63711e79
389093982 <389093982@qq.com>
2019-09-28 22:18:11 +0800

    代码优化111

--- isoft/isoft_iaas_ui/src/components/IGood/PayConfirm.vue
+++ isoft/isoft_iaas_ui/src/components/IGood/PayConfirm.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    SSSSSSSSSSSSSSS
+    <div v-if="good">
+      <Row>
+        <Col span="12"><img :src="good_images[0]" width="100%" height="100%"/></Col>
+        <Col span="12">{{good.good_desc}}</Col>
+      </Row>
+      <Row style="text-align: right;">
+        <Button>付款</Button>
+      </Row>
+    </div>
   </div>
 </template>
 
 <script>
+  import {GetGoodDetail} from "../../api"
+
   export default {
-    name: "PayConfirm"
+    name: "PayConfirm",
+    data(){
+      return {
+        good:null,
+        good_images:[],   // 商品图片
+      }
+    },
+    methods:{
+      refreshGoodDetail:async function () {
+        const result = await GetGoodDetail(this.$route.query.good_id);
+        if(result.status == "SUCCESS"){
+          this.good = result.good;
+          this.good_images = JSON.parse(result.good.good_images);
+        }
+      }
+    },
+    mounted(){
+      this.refreshGoodDetail();
+    }
   }
 </script>
 
