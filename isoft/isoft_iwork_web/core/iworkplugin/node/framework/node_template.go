c71bb8ff15852fc6f6b19702a45ab09fad24bcd8
389093982 <389093982@qq.com>
2019-10-18 20:50:11 +0800

    模板引擎支持 map 类型,进行简化

--- isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
+++ isoft/isoft_iwork_web/core/iworkplugin/node/framework/node_template.go
@@ -5,8 +5,8 @@ import (
 	"isoft/isoft_iwork_web/core/iworkconst"
 	"isoft/isoft_iwork_web/core/iworkmodels"
 	"isoft/isoft_iwork_web/core/iworkplugin/node"
+	"isoft/isoft_iwork_web/core/iworkutil/reflectutil"
 	"isoft/isoft_iwork_web/models"
-	"strings"
 	"text/template"
 )
 
@@ -24,32 +24,22 @@ func (this *stringWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-func parseToDataMap(template_varName string, template_varValue []interface{}) map[string]interface{} {
-	dataMap := make(map[string]interface{}, 0)
-	names := strings.Split(template_varName, ",")
-	if len(names) != len(template_varValue) {
-		panic("miss match length error for template_varName and template_varValue")
+func (this *TemplateNode) getTemplateDataMap() map[string]interface{} {
+	m := make(map[string]interface{}, 0)
+	if dataMap, ok := this.TmpDataMap[iworkconst.COMPLEX_PREFIX+"template_dataMap"].(interface{}); ok {
+		keys, values := reflectutil.InterfaceToMap(dataMap)
+		for index, key := range keys {
+			m[key.Interface().(string)] = values[index].Interface()
+		}
+	} else {
+		panic("template_dataMap is not a map type")
 	}
-	for index, name := range names {
-		dataMap[strings.TrimSpace(name)] = template_varValue[index]
-	}
-	return dataMap
-}
-
-func (this *TemplateNode) convertTemplateVarValue() []interface{} {
-	if values, ok := this.TmpDataMap[iworkconst.COMPLEX_PREFIX+"template_varValue"].([]interface{}); ok {
-		return values
-	} else if value, ok := this.TmpDataMap[iworkconst.COMPLEX_PREFIX+"template_varValue"].(interface{}); ok {
-		return []interface{}{value}
-	}
-	panic("template_varValue is invalid parameter")
+	return m
 }
 
 func (this *TemplateNode) Execute(trackingId string) {
 	template_text := this.TmpDataMap[iworkconst.STRING_PREFIX+"template_text"].(string)
-	template_varName := this.TmpDataMap[iworkconst.STRING_PREFIX+"template_varName"].(string)
-	template_varValue := this.convertTemplateVarValue()
-	dataMap := parseToDataMap(template_varName, template_varValue)
+	dataMap := this.getTemplateDataMap()
 	tmpl, err := template.New("template").Parse(template_text)
 	if err != nil {
 		panic(errors.Wrap(err, "模板语法错误!"))
@@ -67,8 +57,7 @@ func (this *TemplateNode) Execute(trackingId string) {
 func (this *TemplateNode) GetDefaultParamInputSchema() *iworkmodels.ParamInputSchema {
 	paramMap := map[int][]string{
 		1: {iworkconst.STRING_PREFIX + "template_text", "模板文字"},
-		2: {iworkconst.STRING_PREFIX + "template_varName", "模板变量名称"},
-		3: {iworkconst.COMPLEX_PREFIX + "template_varValue", "模板变量值"},
+		2: {iworkconst.COMPLEX_PREFIX + "template_dataMap", "模板变量绑定数据"},
 	}
 	return this.BuildParamInputSchemaWithDefaultMap(paramMap)
 }
