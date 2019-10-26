9df60e4e5b5c2590b650dceebfb9871d93126e5f
389093982 <389093982@qq.com>
2019-10-26 17:05:46 +0800

    优化

--- isoft/isoft_iaas_ui/src/components/ILearning/WhyChooseUs.vue
+++ isoft/isoft_iaas_ui/src/components/ILearning/WhyChooseUs.vue
@@ -1,28 +1,45 @@
 <template>
-  <div style="text-align: center;padding: 50px 0 20px 0;" class="fadeIn">
-    <p style="font-size: 20px;">为什么选择 ILearning?</p>
-    <p style="margin: 20px 0 30px 0;color:#8295B0;font-size:18px;">ISO 27001信息安全认证，免费内容合规性的企业在线学习平台</p>
-    <p>安全 稳定 智能 直播内容防泄密  保护企业数据主权  内容合规性保障 自研流媒体底层技术  世界级大会检验  多云部署 动态负载</p>
-    <Row>
-      <Col span="8">
-        <img src="../../assets/section4-3-1.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
-        <p>太智能啦</p>
-      </Col>
-      <Col span="8">
-        <img src="../../assets/section4-3-3.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
-        <p>太智能啦</p>
-      </Col>
-      <Col span="8">
-        <img src="../../assets/section4-3-1.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
-        <p>太智能啦</p>
-      </Col>
-    </Row>
+  <div>
+    <div style="text-align: center;padding: 50px 0 20px 0;" v-if="showDetail">
+      <p style="font-size: 20px;">为什么选择 ILearning?</p>
+      <p style="margin: 20px 0 30px 0;color:#8295B0;font-size:18px;">ISO 27001信息安全认证，免费内容合规性的企业在线学习平台</p>
+      <p>安全 稳定 智能 直播内容防泄密  保护企业数据主权  内容合规性保障 自研流媒体底层技术  世界级大会检验  多云部署 动态负载</p>
+      <Row>
+        <Col span="8">
+          <img class="fadeIn" src="../../assets/section4-3-1.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
+          <p>太智能啦</p>
+        </Col>
+        <Col span="8">
+          <img class="fadeIn" src="../../assets/section4-3-3.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
+          <p>太智能啦</p>
+        </Col>
+        <Col span="8">
+          <img class="fadeIn" src="../../assets/section4-3-1.png" style="width: 374px;height: 251px;border: 2px solid rgba(0,33,255,0.2);"/>
+          <p>太智能啦</p>
+        </Col>
+      </Row>
+    </div>
+    <div v-else style="text-align: right;font-size: 12px;" @click="showDetail = true">
+      为什么选择 ILearning?
+    </div>
   </div>
+
 </template>
 
 <script>
   export default {
-    name: "WhyChooseUs"
+    name: "WhyChooseUs",
+    data(){
+      return {
+        showDetail:true,
+      }
+    },
+    mounted(){
+      var _this = this;
+      setTimeout(function(){
+        _this.showDetail = false;
+      }, 10000);
+    }
   }
 </script>
 
