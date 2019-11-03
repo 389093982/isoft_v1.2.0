340aec1e73dcd65e431ef487a2ec7acc6faca4a9
389093982 <389093982@qq.com>
2019-11-03 22:09:53 +0800

    样式优化

--- isoft/isoft_iaas_ui/src/components/ILearning/WhyChooseUs.vue
+++ isoft/isoft_iaas_ui/src/components/ILearning/WhyChooseUs.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div style="text-align: center;padding: 120px 0 20px 0;">
-      <h1 style="color: #06ff00;" class="animated bounce infinite">为什么选择 ILearning?</h1>
-      <p style="margin: 50px 0 30px 0;color:#06ff00;font-size:18px;">ISO 27001信息安全认证，免费内容合规性的企业在线学习平台</p>
-      <p style="color: #06ff00;margin-top: 20px;">安全 稳定 智能 直播内容防泄密  保护企业数据主权  内容合规性保障 自研流媒体底层技术  世界级大会检验  多云部署 动态负载</p>
+      <h1 style="color: #ffffff;" class="animated bounce infinite">为什么选择 ILearning?</h1>
+      <p style="margin: 50px 0 30px 0;color:#ffffff;font-size:18px;">ISO 27001信息安全认证，免费内容合规性的企业在线学习平台</p>
+      <p style="color: #ffffff;margin-top: 20px;">安全 稳定 智能 直播内容防泄密  保护企业数据主权  内容合规性保障 自研流媒体底层技术  世界级大会检验  多云部署 动态负载</p>
     </div>
   </div>
 </template>
