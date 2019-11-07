8f5c29fa175e54b554e98c4ddccd36da39199445
389093982 <389093982@qq.com>
2019-11-07 22:24:54 +0800

    样式优化

--- isoft/isoft_iaas_ui/src/components/ILearning/WarmHeartRecommend.vue
+++ isoft/isoft_iaas_ui/src/components/ILearning/WarmHeartRecommend.vue
@@ -3,7 +3,7 @@
     <div class="header">为您精选</div>
     <Row>
       <Col span="12">
-        <video ref="video" width="585px" height="365px" controls autoplay="autoplay">
+        <video ref="video" width="585px" height="365px" controls autoplay="autoplay" src="http://localhost:8086/api/files/75dd3df4-a3db-47ce-948e-4cacd6348d88">
           <source type="video/mp4">
           <source type="video/ogg">
           您的浏览器不支持Video标签。
@@ -11,15 +11,15 @@
       </Col>
       <Col span="12">
         <p style="height: 50px">AAAAAAAA BBBBBBBBBB CCCCCCCCC DDDDDDDDD</p>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
-        <HoverBigImg width="184px" height="98px" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
+        <HoverBigImg width="184px" height="98px" src-img="http://localhost:8086/api/files/510825.gif" style="float: left;margin: 0 10px 10px 0;"/>
       </Col>
     </Row>
 
