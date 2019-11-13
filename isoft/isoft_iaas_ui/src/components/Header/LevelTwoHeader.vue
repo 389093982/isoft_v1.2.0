22ae953d5c6a917797c28068f79feb4efc021343
389093982 <389093982@qq.com>
2019-11-13 22:39:38 +0800

    样式优化

--- isoft/isoft_iaas_ui/src/components/Header/LevelTwoHeader.vue
+++ isoft/isoft_iaas_ui/src/components/Header/LevelTwoHeader.vue
@@ -5,12 +5,14 @@
         <li>
           网站导航 <Icon type="ios-arrow-down" />
           <div class="subArea" style="width: 1000px;right: 110px;">
+            <div class="arrow"></div>
             BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
           </div>
         </li>
         <li>
           热点 APP <Icon type="ios-arrow-down" />
           <div class="subArea" style="width: 1000px;right: 30px;">
+            <div class="arrow"></div>
             BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
           </div>
         </li>
@@ -40,5 +42,23 @@
     border: 1px solid #e1e1e1;
     z-index: 50;
     padding: 20px;
+    display: none;
+  }
+  li:hover{
+    background-color: white;
+  }
+  li:hover .subArea{
+    display: block;
+  }
+  .arrow{
+    position: absolute;
+    right: 25px;
+    top: -10px;
+    width: 0;
+    height: 0;
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent #e1e1e1 transparent transparent;
+    transform: rotate(90deg); /*顺时针旋转90°*/
   }
 </style>
