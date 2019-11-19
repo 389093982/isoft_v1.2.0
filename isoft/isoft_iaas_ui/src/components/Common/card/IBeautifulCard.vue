2baab774bb0542bcec78d853123b80ca178a53e4
389093982 <389093982@qq.com>
2019-11-19 22:59:41 +0800

    样式优化

--- isoft/isoft_iaas_ui/src/components/Common/card/IBeautifulCard.vue
+++ isoft/isoft_iaas_ui/src/components/Common/card/IBeautifulCard.vue
@@ -3,7 +3,7 @@
     <div class="box_area">
       <slot name="header">
         <div class="title">
-          <span style="float: left;">
+          <span class="leftHeaderLeft" style="float: left;">
             <slot name="header_left">{{title}}</slot>
           </span>
           <span style="float: right;">
@@ -47,5 +47,13 @@
    border-bottom: 1px solid #eee;
    text-align: left;
    background: linear-gradient(to right, rgba(255, 0, 0, 0.05), rgba(0, 0, 255, 0.06));
+ }
+ 
+ .leftHeaderLeft{
+    cursor: pointer;
+    transition: all 2s ease-in;
+ }
+ .leftHeaderLeft:hover{
+    color: #00f500;
  }
 </style>
