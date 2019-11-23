3f2c09bd8c2543265c6828c6bb922a6f9308c9b6
zhoupeng <1875112921@qq.com>
2019-11-23 19:36:16 +0800

    作文分享-添加主界面注释

--- isoft/isoft_iaas_ui/src/components/ShareArticle/ShareArticlePlace.vue
+++ isoft/isoft_iaas_ui/src/components/ShareArticle/ShareArticlePlace.vue
@@ -2,26 +2,33 @@
   <div>
 
     <div class="left-pane">
-      共享大厅
+        <!--共享大厅-->
+        <SharingHall></SharingHall>
     </div>
 
     <div class="center-pane">
+      <!--文章编辑-->
       <ArticleEdit>
         <div slot="title">title01</div>
         <div slot="content">content01</div>
       </ArticleEdit>
     </div>
+
     <div class="right-pane">
-      文章列表
+      <!--文章列表-->
+      <ArticleList></ArticleList>
     </div>
+
   </div>
 </template>
 
 <script>
+  import SharingHall from "../ShareArticle/SharingHall"
+  import ArticleList from "../ShareArticle/ArticleList"
   import ArticleEdit from "../ShareArticle/ArticleEdit"
   export default {
     name: "ShareArticlePlace",
-    components:{ArticleEdit}
+    components:{ArticleEdit,SharingHall,ArticleList}
   }
 </script>
 
