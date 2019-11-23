ef42b0d6b3cb27b62a6068e7eefb3b2ce902a193
zhoupeng <1875112921@qq.com>
2019-11-23 22:41:04 +0800

    作文分享-添加工具组件 input_textarea

--- isoft/isoft_iaas_ui/src/components/ShareArticle/ArticleEdit.vue
+++ isoft/isoft_iaas_ui/src/components/ShareArticle/ArticleEdit.vue
@@ -2,15 +2,19 @@
   <div>
       <div>{{onView}}</div>
       <div v-if="article.title" class="SingleArticle-title">{{article.title}}</div>
-      <div v-if="article.title" class="SingleArticle-content">{{article.content}}</div>
+      <div v-if="article.title" class="SingleArticle-content">
+        <input_textarea :content="article.content" :rows="rows" :maxlength="maxlength"></input_textarea>
+      </div>
       <slot name="title"></slot>
       <slot name="content"></slot>
   </div>
 </template>
 
 <script>
+    import input_textarea from "../Utils/input_textarea"
     export default {
-        name: "ArticleEdit",
+      name: "ArticleEdit",
+      components:{input_textarea},
       data(){
           return{
             onView:"正在预览",
@@ -18,9 +22,16 @@
               title:"勇敢的第一步",
               content:"这里是文章内容,这里是文章内容,这里是文章内容," +
               "这里是文章内容,这里是文章内容,这里是文章内容," +
-              "这里是文章内容,这里是文章内容,这里是文章内容,这里是文章" +
-              "内容这里是文章内容,这里是文章内容,这里是文章内容,这里是文章内容"
-            }
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容," +
+              "这里是文章内容,这里是文章内容,这里是文章内容,"
+            },
+            rows:15,
+            maxlength:2000,
           }
       }
     }
---
+++ isoft/isoft_iaas_ui/src/components/Utils/input_textarea.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Input v-model="content" :rows="rows" :maxlength="maxlength" show-word-limit="true" type="textarea" placeholder="Enter something..." style="width: 100%" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "input_textarea",
+    props:{
+      content:String,
+      rows:Number,
+      maxlength:Number,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
